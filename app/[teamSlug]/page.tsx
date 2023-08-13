fc51852300688365cab7a750da623e9081b18a7b
Matheus Dalmolin da Silva <mtsdalmolin@gmail.com>
2023-08-13 01:42:10 -0300

    add other teams and make rounds for each team

--- app/[teamSlug]/page.tsx
+++ app/[teamSlug]/page.tsx
@@ -6,8 +6,6 @@ import AthleteList from './components/athlete-list.client'
 
 const CARTOLA_API = 'https://api.cartola.globo.com/'
 
-const ROUNDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
-
 const PHOTO_SIZE_FORMAT = '220x220'
 
 const TEAM_ROUND_ENDPOINT = (teamId: string) => `time/id/${teamId}/:round`
@@ -57,9 +55,13 @@ function renderedAthleteFactory(athlete: Athlete, captainId: number) {
   }
 }
 
-async function getPlayersTeamData(endpoint: string) {
+function getTeamRounds(teamId: number) {
+  return TEAMS.find(team => team.id === teamId)?.rounds ?? []
+}
+
+async function getPlayersTeamData(endpoint: string, teamId: number ) {
   const roundsData: RoundData[] = await Promise.all(
-    ROUNDS.map(round =>
+    getTeamRounds(teamId).map(round =>
       fetch(`${CARTOLA_API}${endpoint.replace(':round', round.toString())}`)
         .then(res => res.json())
     )
@@ -85,7 +87,6 @@ async function getPlayersTeamData(endpoint: string) {
     })
 
     bench.forEach(benchAthlete => {
-      benchAthlete.apelido === 'Lucas Evangelista' && console.log(benchAthlete)
       if (benchStatistics[benchAthlete.atleta_id]) {
         benchStatistics[benchAthlete.atleta_id].castTimes++
         benchStatistics[benchAthlete.atleta_id].sumOfPoints += calculatePoints(benchAthlete, captainId)
@@ -119,7 +120,7 @@ export default async function Team({ params }: { params: { teamSlug: string } })
     return 'no data'
   }
 
-  const [athletes, bench] = await getPlayersTeamData(TEAM_ROUND_ENDPOINT(teamData.id.toString()))
+  const [athletes, bench] = await getPlayersTeamData(TEAM_ROUND_ENDPOINT(teamData.id.toString()), teamData.id)
 
   return (
     <main className="min-h-screen items-center p-24">
--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,60 @@
-// import Image from 'next/image'
-
 import Link from "next/link"
+
+const ROUNDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+const MIOLLO_AND_THOMAS_ROUNDS = [18]
 
 export const TEAMS = [
   {
     id: 25929996,
     slug: 'sociedade-esportiva-tacu',
-    name: 'Sociedade Esportiva Tacu'
+    name: 'Sociedade Esportiva Tacu',
+    rounds: ROUNDS
   },
   {
     id: 29367702,
     slug: '4-de-maio-futebol-e-cerveja',
-    name: '4 de maio futebol e cerveja'
+    name: '4 de maio futebol e cerveja',
+    rounds: ROUNDS
   },
   {
     id: 15159981,
     slug: 'valeimoveis',
-    name: 'Valeimoveis'
+    name: 'Valeimoveis',
+    rounds: ROUNDS
   },
   {
     id: 112714,
     slug: 'real-egrete',
-    name: 'Real Egrete'
+    name: 'Real Egrete',
+    rounds: ROUNDS
   },
   {
     id: 47843698,
     slug: 'quarai-querencia-querida-fc',
-    name: 'Quaraí Querência Querida FC'
+    name: 'Quaraí Querência Querida FC',
+    rounds: ROUNDS
   },
   {
     id: 28738386,
     slug: 'padariasilvalvto',
-    name: 'Padariasilvalvto'
+    name: 'Padariasilvalvto',
+    rounds: ROUNDS
+  },
+  {
+    id: 48903399,
+    slug: 'os-sem-pernas-pcd',
+    name: 'Os Sem Pernas PCD',
+    rounds: MIOLLO_AND_THOMAS_ROUNDS
+  },
+  {
+    id: 48902734,
+    slug: 'miollo-futebol-e-tristeza',
+    name: 'Miollo Futebol e Tristeza',
+    rounds: MIOLLO_AND_THOMAS_ROUNDS
   },
 ]
 
 export default function Home() {
-
   return (
     <main className="flex min-h-screen flex-col items-center p-12 gap-2">
       {TEAMS.map(team => 
