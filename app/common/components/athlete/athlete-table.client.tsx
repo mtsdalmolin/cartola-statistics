e051d88a495ef5ffd972a86d1fec16f2468e9d56
Matheus Dalmolin da Silva <mtsdalmolin@gmail.com>
2023-08-16 00:38:14 -0300

    move columns to outside react component

--- app/common/components/athlete/athlete-table.client.tsx
+++ app/common/components/athlete/athlete-table.client.tsx
@@ -71,162 +71,159 @@ function ToolbarPositionFilter({ tableObject }: { tableObject: MRT_TableInstance
   )
 }
 
-export function AthleteTable({ athletes }: { athletes: AthleteTableData[] }) {
-  const columns = useMemo<MRT_ColumnDef<AthleteTableData>[]>(
-    () => [
-      {
-        id: 'name',
-        accessorKey: 'name',
-        header: 'Nome',
-        size: 200,
-        filterVariant: 'autocomplete',
-        Cell: ({ renderedCellValue, row }) => (
-          <Box
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              gap: '16px',
-            }}
-          >
-            <Box
-              sx={{
-                position: 'relative'
-              }}
-            >
-              <Image
-                alt={row.original.name}
-                width={50}
-                height={50}
-                src={row.original.photoUrl}
-                style={{ borderRadius: '50%' }}
-              />
-              <Image
-                alt={row.original.club}
-                width={20}
-                height={20}
-                src={row.original.clubBadgeUrl}
-                style={{ position: 'absolute', bottom: 0, right: 0 }}
-              />
-            </Box>
-            <span>{renderedCellValue}</span>
-          </Box>
-        ),
-      },
-      {
-        id: 'highestPoint',
-        accessorKey: 'highestPoint',
-        header: 'MP',
-        filterVariant: 'range-slider',
-      },
-      {
-        id: 'position',
-        accessorKey: 'position',
-        header: 'Posição',
-        filterVariant: 'multi-select',
-      },
-      {
-        id: 'sumOfPlayedMinutes',
-        accessorKey: 'sumOfPlayedMinutes',
-        header: 'Min. jogados',
-        filterVariant: 'range-slider',
-      },
-      {
-        id: 'averageMinutesPerRound',
-        accessorKey: 'averageMinutesPerRound',
-        header: 'MPM/R',
-        filterVariant: 'range-slider',
-        Cell: ({ cell }) => cell.getValue<number>().toFixed(1)
-      },
-      {
-        id: 'pointsAverage',
-        accessorKey: 'pointsAverage',
-        header: 'Média',
-        filterVariant: 'range-slider',
-        Cell: ({ cell }) => cell.getValue<number>().toFixed(1)
-      },
-      {
-        id: 'pointsAverageHome',
-        accessorKey: 'pointsAverageHome',
-        header: 'MPM/R',
-        filterVariant: 'range-slider',
-        Cell: ({ cell }) => cell.getValue<number>().toFixed(1)
-      },
-      {
-        id: 'pointsAverageAway',
-        accessorKey: 'pointsAverageAway',
-        header: 'MPV/R',
-        filterVariant: 'range-slider',
-        Cell: ({ cell }) => cell.getValue<number>().toFixed(1)
-      },
-      {
-        id: 'finishes',
-        accessorKey: 'finishes',
-        header: 'Finalizações',
-        filterVariant: 'range-slider',
-        Cell: ({ cell }) => cell.getValue<number>().toFixed(1)
-      },
-      {
-        id: 'finishesToScore',
-        accessorKey: 'finishesToScore',
-        header: 'FPG',
-        filterVariant: 'range-slider',
-        Cell: ({ cell }) => cell.getValue<number>().toFixed(1)
-      },
-      {
-        id: 'goals',
-        accessorKey: 'goals',
-        header: 'Gols',
-        filterVariant: 'range-slider',
-      },
-      {
-        id: 'defenses',
-        accessorKey: 'defenses',
-        header: 'Defesas',
-        filterVariant: 'range-slider',
-      },
-      {
-        id: 'goalsAgainst',
-        accessorKey: 'goalsAgainst',
-        header: 'Gols sofridos',
-        filterVariant: 'range-slider',
-      },
-      {
-        id: 'defensesToSufferGoal',
-        accessorKey: 'defensesToSufferGoal',
-        header: 'DPG',
-        filterVariant: 'range-slider',
-        Cell: ({ cell }) => cell.getValue<number>().toFixed(1)
-      },
-      {
-        id: 'minutesToScore',
-        accessorKey: 'minutesToScore',
-        header: 'MPG',
-        filterVariant: 'range-slider',
-        Cell: ({ cell }) => cell.getValue<number>().toFixed(1)
-      },
-      {
-        id: 'victoriesAverage',
-        accessorKey: 'victoriesAverage',
-        header: 'Vitórias %',
-        filterVariant: 'range-slider',
-        Cell: ({ cell }) => cell.getValue<number>().toFixed(1)
-      },
-      {
-        id: 'castTimes',
-        accessorKey: 'castTimes',
-        header: 'Escalações',
-        filterVariant: 'range-slider',
-      },
-      {
-        id: 'captainTimes',
-        accessorKey: 'captainTimes',
-        header: 'Vezes Cap.',
-        filterVariant: 'range-slider',
-      },
-    ],
-    [],
-  );
+const columns: MRT_ColumnDef<AthleteTableData>[] = [
+  {
+    id: 'name',
+    accessorKey: 'name',
+    header: 'Nome',
+    size: 200,
+    filterVariant: 'autocomplete',
+    Cell: ({ renderedCellValue, row }) => (
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: '16px',
+        }}
+      >
+        <Box
+          sx={{
+            position: 'relative'
+          }}
+        >
+          <Image
+            alt={row.original.name}
+            width={50}
+            height={50}
+            src={row.original.photoUrl}
+            style={{ borderRadius: '50%' }}
+          />
+          <Image
+            alt={row.original.club}
+            width={20}
+            height={20}
+            src={row.original.clubBadgeUrl}
+            style={{ position: 'absolute', bottom: 0, right: 0 }}
+          />
+        </Box>
+        <span>{renderedCellValue}</span>
+      </Box>
+    ),
+  },
+  {
+    id: 'highestPoint',
+    accessorKey: 'highestPoint',
+    header: 'MP',
+    filterVariant: 'range-slider',
+  },
+  {
+    id: 'position',
+    accessorKey: 'position',
+    header: 'Posição',
+    filterVariant: 'multi-select',
+  },
+  {
+    id: 'sumOfPlayedMinutes',
+    accessorKey: 'sumOfPlayedMinutes',
+    header: 'Min. jogados',
+    filterVariant: 'range-slider',
+  },
+  {
+    id: 'averageMinutesPerRound',
+    accessorKey: 'averageMinutesPerRound',
+    header: 'MPM/R',
+    filterVariant: 'range-slider',
+    Cell: ({ cell }) => cell.getValue<number>().toFixed(1)
+  },
+  {
+    id: 'pointsAverage',
+    accessorKey: 'pointsAverage',
+    header: 'Média',
+    filterVariant: 'range-slider',
+    Cell: ({ cell }) => cell.getValue<number>().toFixed(1)
+  },
+  {
+    id: 'pointsAverageHome',
+    accessorKey: 'pointsAverageHome',
+    header: 'MPM/R',
+    filterVariant: 'range-slider',
+    Cell: ({ cell }) => cell.getValue<number>().toFixed(1)
+  },
+  {
+    id: 'pointsAverageAway',
+    accessorKey: 'pointsAverageAway',
+    header: 'MPV/R',
+    filterVariant: 'range-slider',
+    Cell: ({ cell }) => cell.getValue<number>().toFixed(1)
+  },
+  {
+    id: 'finishes',
+    accessorKey: 'finishes',
+    header: 'Finalizações',
+    filterVariant: 'range-slider',
+    Cell: ({ cell }) => cell.getValue<number>().toFixed(1)
+  },
+  {
+    id: 'finishesToScore',
+    accessorKey: 'finishesToScore',
+    header: 'FPG',
+    filterVariant: 'range-slider',
+    Cell: ({ cell }) => cell.getValue<number>().toFixed(1)
+  },
+  {
+    id: 'goals',
+    accessorKey: 'goals',
+    header: 'Gols',
+    filterVariant: 'range-slider',
+  },
+  {
+    id: 'defenses',
+    accessorKey: 'defenses',
+    header: 'Defesas',
+    filterVariant: 'range-slider',
+  },
+  {
+    id: 'goalsAgainst',
+    accessorKey: 'goalsAgainst',
+    header: 'Gols sofridos',
+    filterVariant: 'range-slider',
+  },
+  {
+    id: 'defensesToSufferGoal',
+    accessorKey: 'defensesToSufferGoal',
+    header: 'DPG',
+    filterVariant: 'range-slider',
+    Cell: ({ cell }) => cell.getValue<number>().toFixed(1)
+  },
+  {
+    id: 'minutesToScore',
+    accessorKey: 'minutesToScore',
+    header: 'MPG',
+    filterVariant: 'range-slider',
+    Cell: ({ cell }) => cell.getValue<number>().toFixed(1)
+  },
+  {
+    id: 'victoriesAverage',
+    accessorKey: 'victoriesAverage',
+    header: 'Vitórias %',
+    filterVariant: 'range-slider',
+    Cell: ({ cell }) => cell.getValue<number>().toFixed(1)
+  },
+  {
+    id: 'castTimes',
+    accessorKey: 'castTimes',
+    header: 'Escalações',
+    filterVariant: 'range-slider',
+  },
+  {
+    id: 'captainTimes',
+    accessorKey: 'captainTimes',
+    header: 'Vezes Cap.',
+    filterVariant: 'range-slider',
+  },
+]
 
+export function AthleteTable({ athletes }: { athletes: AthleteTableData[] }) {
   const table = useMantineReactTable({
     columns,
     data: athletes,
