ebfe3a30002eff318f726248d07eda16b9eb2b1d
Matheus Dalmolin da Silva <mtsdalmolin@gmail.com>
2023-08-19 12:40:33 -0300

    add button component

---
+++ app/common/components/button.client.tsx
@@ -0,0 +1,20 @@
+import { ReactElement } from "react"
+
+interface ButtonProps {
+  children: string | ReactElement
+  onClick: () => void
+}
+
+export function Button({
+  children,
+  onClick,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      className="bg-sky-600 text-cyan-50 py-2 px-4 rounded-md hover:opacity-95"
+      onClick={onClick}
+      {...props}
+    >{children}</button>
+  )
+}
