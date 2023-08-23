fa65739eead694084fc9a32045233558b651e960
Matheus Dalmolin da Silva <mtsdalmolin@gmail.com>
2023-08-23 18:49:00 -0300

    add redirect to market in home

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import Link from "next/link"
-import { TEAMS } from "./constants/data"
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-12 gap-2">
-      <Link href="/mercado">Mercado</Link>
-      {TEAMS.map(team => 
-        <Link key={team.id} href={`/${team.slug}`}>{team.name}</Link>
-      )}
-    </main>
+    <section>
+      conteúdo aqui
+    </section>
   )
 }
---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: '/'
+}
+
+export function middleware() {
+  return NextResponse.redirect(new URL(`${process.env.NEXT_API_BASE_URL}/mercado`))
+}
