d5830410f34f672dd6968f73fcdbc566f472436f
Matheus Dalmolin da Silva <mtsdalmolin@gmail.com>
2023-08-23 13:59:36 -0300

    add revalidation time in cartola request method

--- app/services/cartola-api.ts
+++ app/services/cartola-api.ts
@@ -1,5 +1,11 @@
 export const CARTOLA_API = 'https://api.cartola.globo.com/'
 
+const REVALIDATION_TIME_IN_SECONDS = 5 * 60
+
 export function request(endpoint: string) {
-  return fetch(`${CARTOLA_API}${endpoint}`)
+  return fetch(`${CARTOLA_API}${endpoint}`, {
+    next: {
+      revalidate: REVALIDATION_TIME_IN_SECONDS
+    }
+  })
 }
