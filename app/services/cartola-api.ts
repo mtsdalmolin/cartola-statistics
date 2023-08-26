1c0a65569e20789af1be7de17e3ab8101730a9db
Matheus Dalmolin da Silva <mtsdalmolin@gmail.com>
2023-08-25 22:05:33 -0300

    change the way options are passed in request method

--- app/services/cartola-api.ts
+++ app/services/cartola-api.ts
@@ -10,10 +10,11 @@ export const ENDPOINTS = {
 const REVALIDATION_TIME_IN_SECONDS = 5 * 60
 
 export function request(endpoint: string, options: RequestInit = {}) {
-  return fetch(`${CARTOLA_API}${endpoint}`, {
+  const requestOptions = options ?? {
     next: {
-      revalidate: options?.next?.revalidate ?? REVALIDATION_TIME_IN_SECONDS,
-    },
-    ...options
-  }).then(res => res.json())
+      revalidate: REVALIDATION_TIME_IN_SECONDS,
+    }
+  }
+
+  return fetch(`${CARTOLA_API}${endpoint}`, requestOptions).then(res => res.json())
 }
