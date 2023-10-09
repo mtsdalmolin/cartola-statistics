1d2a8a7f16f45b87acd2f984a3c39ebc845ebd4a
Matheus Dalmolin da Silva <mtsdalmolin@gmail.com>
2023-10-09 19:31:51 -0300

    remove redirect middleware from homepage

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,7 @@
-import { NextResponse } from 'next/server'
+// import { NextResponse } from 'next/server'
 
-export const config = {
-  matcher: '/'
-}
+// export const config = {
+//   matcher: '/'
+// }
 
-export function middleware() {
-  return NextResponse.redirect(new URL(`${process.env.NEXT_API_BASE_URL}/mercado`))
-}
+export function middleware() {}
