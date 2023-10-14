956ec96ba6ae452323711cc6534e48ce1384c4dd
Matheus Dalmolin da Silva <mtsdalmolin@gmail.com>
2023-10-14 19:42:09 -0300

    create a generic round stats details

---
+++ app/common/components/statistics/details/animated-stats-round-matches-result.tsx
@@ -0,0 +1,56 @@
+import { RoundMatchesData } from '@/app/services/types'
+
+import { Flex } from '../../flex'
+import { MatchVersus } from '../../match-versus'
+
+export function AnimatedStatsRoundMatchesResult({
+  clubId,
+  matchesData,
+  statRounds,
+  statText,
+  isAnimated = false
+}: {
+  clubId: number
+  matchesData: RoundMatchesData
+  statRounds: { [key: string]: number }
+  statText: string
+  isAnimated?: boolean
+}) {
+  return (
+    <Flex
+      className={`min-w-[130px] w-min max-h-60 ${isAnimated ? 'flex-nowrap overflow-hidden' : ''}`}
+      direction="column"
+    >
+      <div className={isAnimated ? 'h-max animate-scrollVertically hover:pause-animation' : ''}>
+        {Object.entries(statRounds).map(([roundId, statValue]) => (
+          <Flex key={roundId} direction="column">
+            <Flex className="text-xs" justify="between">
+              <span>Rodada {roundId}</span>
+              <span>
+                ({statValue} {statText})
+              </span>
+            </Flex>
+            <Flex>
+              <MatchVersus match={matchesData[roundId][clubId]} badgeSize={25} />
+            </Flex>
+          </Flex>
+        ))}
+        {isAnimated
+          ? Object.entries(statRounds).map(([roundId, statValue]) => (
+              <Flex aria-hidden="true" key={roundId} direction="column">
+                <Flex className="text-xs" justify="between">
+                  <span>Rodada {roundId}</span>
+                  <span>
+                    ({statValue} {statText})
+                  </span>
+                </Flex>
+                <Flex>
+                  <MatchVersus match={matchesData[roundId][clubId]} badgeSize={25} />
+                </Flex>
+              </Flex>
+            ))
+          : null}
+      </div>
+    </Flex>
+  )
+}
