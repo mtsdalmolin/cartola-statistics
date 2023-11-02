794413521aa22a46cbf5a2b1f5dbc443836a1e36
Matheus Dalmolin da Silva <mtsdalmolin@gmail.com>
2023-11-02 04:06:30 -0300

    add metadata in home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,17 +3,35 @@ import '@/app/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import edcBrand from '@/public/logo/twitter-card.png'
 import { Analytics } from '@vercel/analytics/react'
 
 import { Main } from './common/components/main/main.client'
 import { TeamInfoContextProvider } from './common/contexts/team-info-context.client'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const dynamic = 'force-dynamic'
 
+const title = 'Estatísticas do Cartola'
+const description = 'Analise as estatísticas das suas escalações e veja o restrospecto do ano.'
 export const metadata: Metadata = {
-  title: 'Estatísticas do Cartola',
-  description: 'Analise as estatísticas das suas escalações e veja o restrospecto do ano.'
+  title,
+  description,
+  openGraph: {
+    type: 'website',
+    title,
+    description,
+    images: [edcBrand.src]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title,
+    description,
+    creator: '@mtsdalmolin',
+    creatorId: '1686521332597477400',
+    images: [edcBrand.src]
+  }
 }
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
