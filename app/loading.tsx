1fc98141ba115ea287cccbcbd7790ff0a3f726a3
Matheus Dalmolin da Silva <mtsdalmolin@gmail.com>
2023-11-05 02:12:13 -0300

    change default loader to edc bouncing logo

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,13 +1,12 @@
 'use client'
 
-import { Loader } from '@mantine/core'
-
 import { Flex } from './common/components/flex'
+import { LoadingFallback } from './common/components/loading-fallback'
 
 export default function LoadingPage() {
   return (
     <Flex className="m-auto min-h-screen" align="center" justify="center">
-      <Loader color="#7ae1bf" />
+      <LoadingFallback message="Carregando..." />
     </Flex>
   )
 }
