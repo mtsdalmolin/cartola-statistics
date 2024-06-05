90acdc8fac60daf453d56d49087de3e465ee4208
Matheus Dalmolin da Silva <mtsdalmolin@gmail.com>
2024-06-04 23:40:11 -0300

    add round 7 of season 2024

--- app/constants/data.ts
+++ app/constants/data.ts
@@ -14,7 +14,7 @@ export const SEASONS: Record<
     SECOND_TURN_ROUNDS
   },
   2024: {
-    FIRST_TURN_ROUNDS: [1, 2, 3, 4, 5, 6],
+    FIRST_TURN_ROUNDS: [1, 2, 3, 4, 5, 6, 7],
     SECOND_TURN_ROUNDS: []
   }
 }
