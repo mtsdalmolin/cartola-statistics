ff20e2fb7a02c8aafecacd6f2f78bf88e4f111a8
Matheus Dalmolin da Silva <mtsdalmolin@gmail.com>
2025-03-30 01:41:05 -0300

    add get token route

---
+++ app/(server)/api/get-token/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server'
+
+import { sql } from '@vercel/postgres'
+
+export const metadata = {}
+
+export async function GET() {
+  const result = await sql`
+    SELECT jwt
+    FROM cartola_auth_token
+    ORDER BY created_at DESC
+    FETCH FIRST 1 ROWS ONLY
+  `
+
+  if (!result.rows)
+    return NextResponse.json({ message: 'Failed to fetch token' }, { status: 400 })
+
+  return NextResponse.json({ jwt: result.rows[0].jwt }, { status: 200 })
+}
