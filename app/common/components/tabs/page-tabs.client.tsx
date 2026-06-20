802e237e41e88a34c3bd6bd2906d48edb4bc5994
Matheus Dalmolin da Silva <mtsdalmolin@gmail.com>
2026-06-20 00:38:31 -0300

    add world cup tab in the home screen

--- app/common/components/tabs/page-tabs.client.tsx
+++ app/common/components/tabs/page-tabs.client.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
-import { Tabs } from '@mantine/core'
+import { Badge, Tabs } from '@mantine/core'
 
 export function PageClientTabs() {
   const pathname = usePathname()
@@ -11,7 +11,25 @@ export function PageClientTabs() {
   return (
     <Tabs value={pathname}>
       <nav>
-        <Tabs.List position="apart" grow>
+        <Tabs.List position="apart" grow className="h-9">
+          <Link href="/copa">
+            <Tabs.Tab
+              className="
+              data-[active]:border-palette-primary-500
+              data-[active]:hover:border-palette-neutral-100
+              hover:border-palette-neutral-100
+                hover:bg-transparent
+              "
+              value="/copa"
+            >
+              <>
+                Copa do Mundo{' '}
+                <Badge size="xs" radius="xs">
+                  Novo!
+                </Badge>
+              </>
+            </Tabs.Tab>
+          </Link>
           <Link href="/">
             <Tabs.Tab
               className="
